c6c23bfadfc990cf9413ffb4a03875b1389085d5
satyam <satyampayal10@gmail.com>
2024-12-05 17:48:17 +0530

    Deployment done

--- client/src/config/axiosIns.js
+++ client/src/config/axiosIns.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-const BASE_URL="http://localhost:3000";
-// const BASE_URL="https://sssr-xfzj.onrender.com/api/v1";
+// const BASE_URL="http://localhost:3000";
+const BASE_URL="https://rescunet-01-org-3.onrender.com";
 
 const axiosInstance=axios.create();
 
-axiosInstance.defaults.baseURL=BASE_URL;
+axiosInstance.defaults.baseURL=BASE_URL || "http://localhost:3000";
 axiosInstance.defaults.withCredentials=true;
 axiosInstance.defaults.timeout=10000000000000;
 export default axiosInstance;
